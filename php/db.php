d27b3fa6cfb7c06d2a5c31188821ceaa0f7386f9
Ibraheem Abdullah <ibraheem.2abdullah2005@gmail.com>
2026-05-13 14:16:17 +0300

    Fixed database connection

--- php/db.php
+++ php/db.php
@@ -1,15 +1,20 @@
 <?php
-$host     = "localhost";
-$user     = "root";     
-$password = "";
-$dbname   = "bookmark_manager";
+require_once __DIR__ . '/../vendor/autoload.php';
 
-$conn = new mysqli($host, $user, $password, $dbname);
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASS'];
+$db   = $_ENV['DB_NAME'];
+
+$conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
-    // Something went wrong
-    die("❌ Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 
 $conn->set_charset("utf8mb4");
-?>
